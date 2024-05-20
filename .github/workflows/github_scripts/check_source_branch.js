1b3c420daaa6dfdd6609be3d9386dab4287cdac8
Jacopo <jacopo.carlini@gmail.com>
2024-05-20 12:17:14 +0200

    fix

--- .github/workflows/github_scripts/check_source_branch.js
+++ .github/workflows/github_scripts/check_source_branch.js
@@ -3,5 +3,4 @@ module.exports = ({github, context, core}) => {
     if (context.head_ref !== 'next' && context.head_ref.startsWith('hotfix')) {
         core.setFailed(`Merge requests to main branch are only allowed from next branch or branches with a name that starts with hotfix`);
     }
-    core.info('Source is OK')
 }
